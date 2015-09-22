97e4b5903248062c247906106eb489e2ca753c7c
Sébastien Varlet <sebastien.varlet@gmail.com>
2015-09-22 13:58:14 +0100

    Injects HTTPoison instead of using HTTPoison.Base.

--- lib/exowm/query.ex
+++ lib/exowm/query.ex
@@ -1,13 +1,13 @@
 defmodule Exowm.Query do
   use HTTPoison.Base
 
-  @endpoint "http://api.openweathermap.org/data/2.5"
+  @url "http://api.openweathermap.org/data/2.5"
 
-  def weather_in(city, country_code, options \\ []) do
+  def weather_in(city, country_code, options \\ [], http_module \\ HTTPoison) do
     params = options
              |> enforce_json
              |> specify_location(city, country_code)
-    %HTTPoison.Response{body: body} = get!("/weather", [], [params: params])
+    %HTTPoison.Response{body: body} = http_module.get!("/weather", [], [params: params])
     Exowm.CurrentWeather.from body
   end
 
@@ -19,8 +19,8 @@ defmodule Exowm.Query do
     Keyword.put(options, :q, "#{city},#{country_code}")
   end
 
-  def process_url(url) do
-    @endpoint <> url
+  def url_of(endpoint) do
+    @url <> endpoint
   end
 
   def process_response_body(body) do
