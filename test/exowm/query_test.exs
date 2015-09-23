2e8b8c21256ec9c3edb0dd826cf1a20eeafdc9aa
Sébastien Varlet <sebastien.varlet@gmail.com>
2015-09-23 15:23:33 +0100

    Adds a unit test to ensure that `Query.weather_in` enforces the json format.

--- test/exowm/query_test.exs
+++ test/exowm/query_test.exs
@@ -7,15 +7,21 @@ defmodule Exowm.QueryTest do
     assert [] == headers
   end
 
-  Test "weather_in sends a request to the right url" do
+  test "weather_in sends a request to the right url" do
     Exowm.Query.weather_in("london", "uk", [], Exowm.HttpSpy)
     assert_received {:get!, url, _, _}
-    assert url == Exowm.Query.api_url <> "/weather"
+    assert url == Exowm.Query.url_for "/weather"
   end
 
   test "weather_in injects the city and the country_code into the provided params" do
     Exowm.Query.weather_in("london", "uk", [], Exowm.HttpSpy)
     assert_received {:get!, _, _, params: params}
     assert {:q, "london,uk"} == List.keyfind(params, :q, 0)
+  end
+
+  test "weather_in enforces the json format via the `mode` request parameter" do
+    Exowm.Query.weather_in "london", "uk", [], Exowm.HttpSpy
+    assert_received {:get!, _, _, params: params}
+    assert {:mode, "json"} == List.keyfind(params, :mode, 0)
   end
 end
