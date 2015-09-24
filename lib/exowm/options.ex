5696afb299ee899ab01c33ac00ec834df33e72ac
Sébastien Varlet <sebastien.varlet@gmail.com>
2015-09-24 18:37:31 +0100

    Adds documentation for several functions.

--- lib/exowm/options.ex
+++ lib/exowm/options.ex
@@ -1,10 +1,21 @@
 defmodule Exowm.Options do
+  @doc """
+  Appends {:units, \"metric\"} to the provided options in order to get results
+  expressed in the metric system.
+  """
   def in_metric_units(options \\ []), do: Keyword.put(options, :units, "metric")
-  
+
+  @doc """
+  Appends {:units, \"imperial\"} to the provided options in order to get results
+  expressed in the metric system.
+  """
   def in_imperial_units(options \\ []), do: Keyword.put(options, :units, "imperial")
 
+  @doc """
+  Appends {:units, \"standard\"} to the provided options in order to get results
+  expressed in the metric system.
+  """
   def in_standard_units(options \\ []), do: Keyword.put(options, :units, "standard")
-
 
   @external_resource langs_file = Path.join(__DIR__, "langs.txt")
 
@@ -14,7 +25,7 @@ defmodule Exowm.Options do
   |> Stream.map(&String.split(&1, "=", parts: 2, trim: true))
   |> Enum.each(fn([name, abbrev]) ->
      @doc """
-       to_#{name} will add {:lang, #{abbrev}} to the provided Keyword.
+     Appends {:lang, #{abbrev}} to the provided options in order to get results in #{name}.
      """
      def unquote(String.to_atom("in_#{name}"))(options \\ []) do
        Keyword.put(options, :lang, unquote(abbrev))
